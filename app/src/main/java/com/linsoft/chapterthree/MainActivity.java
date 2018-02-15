662c0ca9a324eee35e1a92e750ef65b47d1502c2
Markus Setya Budi Soetikno <markus.setya.budi.s@polytron.co.id>
2018-02-15 16:48:17 +0700

    Add button to switch activity function

--- app/src/main/java/com/linsoft/chapterthree/MainActivity.java
+++ app/src/main/java/com/linsoft/chapterthree/MainActivity.java
@@ -1,7 +1,10 @@
 package com.linsoft.chapterthree;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonNext = (Button) findViewById(R.id.btnNext);
+        buttonNext.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(v.getContext(), NextActivity.class);
+                v.getContext().startActivity(i);
+            }
+        });
+
+        Button buttonTab = (Button) findViewById(R.id.btnTab);
+        buttonTab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(v.getContext(), TabbedActivity.class);
+                v.getContext().startActivity(i);
+            }
+        });
+
     }
 }
