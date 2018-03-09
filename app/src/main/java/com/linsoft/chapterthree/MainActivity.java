df67c32ab84de493e5b85f8d9c5e2c24b93659a7
Markus Lin <markus1268@gmail.com>
2018-03-09 20:27:02 +0700

    Adding Log

--- app/src/main/java/com/linsoft/chapterthree/MainActivity.java
+++ app/src/main/java/com/linsoft/chapterthree/MainActivity.java
@@ -5,8 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    String tag = "Lifecycle Step";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +33,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Log.d(tag, "In the onCreate() event");
+    }
+
+    public void onStart()
+    {
+        super.onStart();
+        Log.d(tag, "In the onStart() event");
+    }
+
+    public void onRestart()
+    {
+        super.onRestart();
+        Log.d(tag, "In the onRestart() event");
+    }
+
+    public void onResume()
+    {
+        super.onResume();
+        Log.d(tag, "In the onResume() event");
+    }
+
+    public void onPause()
+    {
+        super.onPause();
+        Log.d(tag, "In the onPause() event");
+    }
+
+    public void onStop()
+    {
+        super.onStop();
+        Log.d(tag, "In the onStop() event");
+    }
+
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Log.d(tag, "In the onDestroy() event");
     }
 }
